6e2685df5dcecf9abdd57a2b82aeb407e667e7f2
Rahul <rahuldevan2019@gmail.com>
2024-03-02 22:54:52 +0530

    small bug fixed

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,9 +2,7 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import Spinner from '../components/Spinner.jsx'
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai'
-import { BsInfoCircle } from 'react-icons/bs'
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md'
+import { MdOutlineAddBox } from 'react-icons/md'
 import BookCard from '../components/home/BookCard.jsx';
 import BookTable from '../components/home/BookTable.jsx';
 
